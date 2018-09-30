73b74cf41ea2aa1da7c3237dc5362104c48cbaa4
Chris Jones <chrisjones0@yahoo.co.uk>
2018-09-30 15:41:26 +0100

    Map features added

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Christopher Jones on 30/09/2018.
+//  Copyright © 2018 Christopher Jones. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
