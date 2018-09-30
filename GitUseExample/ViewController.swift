71e91ecbd674e0275b0fd96b62cb7cb8b421aafb
Chris Jones <chrisjones0@yahoo.co.uk>
2018-09-30 15:25:15 +0100

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Christopher Jones on 30/09/2018.
+//  Copyright © 2018 Christopher Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
